4feabacb8d1c84ab465278bc540a06d7919e72d6
Avi-ec2-avivarma <avinash.varma4464@gmail.com>
2013-02-04 15:55:43 +0000

    MVP ready.
    -- Country, state, city, pin validation done.

--- lib/validator/validator_v1.rb
+++ lib/validator/validator_v1.rb
@@ -2,40 +2,61 @@ require 'yaml'
 
 class AddressValidator
   class << self
+    @@data = YAML.load_file "../../input.yaml"
     
     #----------------------------------------------------------------------------
-    def load_knowledge_base
-      data = YAML.load_file "../../input.yaml"
-      p data
-    end
-    
-    
-    #----------------------------------------------------------------------------
-    def get_address(address)
+    def init
+      sample_address = {
+                         "address1" => "59,abc apt",
+                         "address2" => "test hights",
+                         "city"     => "tcity",
+                         "state"    => "tokyo",
+                         "pin"      => "1400003",
+                         "country"  => "japan" 
+                        }
+                      
       
+      validate(sample_address)
+      #p data
     end
-    
+  
+    #----------------------------------------------------------------------------
+    def validate_country(a_country)
+      data.any? {|countries| countries.any? {|country| country == a_country} }
+    end
+      
+    #----------------------------------------------------------------------------
+    def validate_state(state, country)
+      data[country].include?(state)
+    end
     
     #----------------------------------------------------------------------------
-    def get_city(state)
+    def validate_city(city, state, country)
+      data[country][state].include?(city)
     end
-    
     
     #----------------------------------------------------------------------------
-    def get_state(country)
+    def validate_pin(country, pin)
+      data[country]["pin_length"] == pin.length
     end
-    
-    
-    #----------------------------------------------------------------------------
-    def build_address(address)
-    end
-    
     
     #----------------------------------------------------------------------------
     def validate(address)
+      p "country : " + validate_country(address["country"]).to_s
+      p "state : "   + validate_state(address["state"], address["country"]).to_s
+      p "city : "    + validate_city(address["city"], address["state"], address["country"]).to_s
+      p "pin : "     + validate_pin(address["country"], address["pin"]).to_s
     end
-  end
-   
+    
+    #----------------------------------------------------------------------------
+    def data
+      return @@data
+    end
+  end   
   
-  load_knowledge_base
+  s_time = Time.now
+  init
+  e_time = Time.now
+  delta = e_time - s_time
+  p "Time taken : " + delta.to_s
 end
